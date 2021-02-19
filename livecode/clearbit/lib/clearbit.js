9a80000581741b80c6bef2567369094a29fb6bf6
Matheus Penchel <matheus.penchel@gmail.com>
2021-02-19 17:16:57 -0300

    Added the API's endpoint

--- livecode/clearbit/lib/clearbit.js
+++ livecode/clearbit/lib/clearbit.js
@@ -1,2 +1,6 @@
 const authorization = "Bearer sk_e7fd564fc3c1eaffae41e71dfa576879";
 // TODO
+
+// "Authorization: Bearer sk_e7fd564fc3c1eaffae41e71dfa576879"
+
+const baseUrl = "https://person.clearbit.com/v2/combined/find?email="
