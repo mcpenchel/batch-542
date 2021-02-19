4589e70663a0d6e0b88c3bcb46f2ed13d681e4cd
Matheus Penchel <matheus.penchel@gmail.com>
2021-02-19 18:23:38 -0300

    Refactored the code with some functions

--- livecode/clearbit/lib/clearbit.js
+++ livecode/clearbit/lib/clearbit.js
@@ -1,12 +1,8 @@
 const authorization = "Bearer sk_e7fd564fc3c1eaffae41e71dfa576879";
-// TODO
-
-// "Authorization: Bearer sk_e7fd564fc3c1eaffae41e71dfa576879"
-
 const baseUrl = "https://person.clearbit.com/v2/combined/find?email="
 
 // 1. find the relevant elements and query 
-//select them and store them in variables
+// select them and store them in variables
  
 const form = document.querySelector("#clearbitForm");
 const input = document.querySelector("#clearbitEmail");
@@ -15,37 +11,33 @@ const email = document.querySelector("#userEmail");
 const bio = document.querySelector("#userBio");
 const location = document.querySelector("#userLocation");
 
-// 2. Attach an event listener to  the form (submit)
-form.addEventListener('submit', (event) => {
-  // 3. Prevent the default behavior of our event listner
-  event.preventDefault();
+const populateUserInfo = (data) => {
+  console.log(data);
+  // 5. Fill users info with the API response 
+  if (data.person) {
+    name.innerText = data.person.name.fullName;
+    email.innerText = data.person.email;
+    bio.innerText = data.person.bio;
+    location.innerText = data.person.location;
+  };
+}
 
-
+const makeAjaxRequestToClearbitAPI = () => {
+  // 4. Make and AJAX GET REQUEST 
   fetch(`${baseUrl}${input.value}`, {
     headers: {
       Authorization: authorization
     }
   })
     .then(response => response.json())
-    .then((data) => {
+    .then(populateUserInfo);
+}
 
-      console.log(data)
-      if (data.person) {
-        name.innerText = data.person.name.fullName
-        email.innerText = data.person.email
-        bio.innerText = data.person.bio
-        location.innerText = data.person.location
-      };
+const handleFormSubmission = (event) => {
+// 3. Prevent the default behavior of our event listner
+  event.preventDefault();
+  makeAjaxRequestToClearbitAPI();
+}
 
-    });
-
-});
-
-
-
-// 4. Make and AJAX GET REQUEST 
-
-
-// 5. Fill users info with the API response 
-
-// 6. 
+// 2. Attach an event listener to  the form (submit)
+form.addEventListener('submit', handleFormSubmission);
