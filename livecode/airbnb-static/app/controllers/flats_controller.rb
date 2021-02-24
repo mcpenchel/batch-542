e8989efcf77b97b40743888bfe21127b48ba6dea
Matheus Penchel <matheus.penchel@gmail.com>
2021-02-24 18:42:53 -0300

    Refactored the controller to not repeat myself

--- livecode/airbnb-static/app/controllers/flats_controller.rb
+++ livecode/airbnb-static/app/controllers/flats_controller.rb
@@ -1,17 +1,22 @@
 require "open-uri"
 
-
 class FlatsController < ApplicationController
+
+  # Everything uppercase, so Ruby knows it's a constant!
+  FLATS_API_URL = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+
+  before_action :load_flats_db
+
   def index
-    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
-    @flats = JSON.parse(open(url).read)
   end
 
   def show
-    url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
-    flats = JSON.parse(open(url).read)
+    @flat = @flats.find{|flat| flat["id"] == params[:id].to_i}
+  end
 
-    @flat = flats.find{|flat| flat["id"] == params[:id].to_i}
+  private
+  def load_flats_db
+    @flats = JSON.parse(open(FLATS_API_URL).read)
   end
 end
 
